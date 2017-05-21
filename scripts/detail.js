1537ac343e1cbba509b121067ec4af6d16be3f24
Christian Albiez <calbiez@hsr.ch>
2017-05-21 16:37:53 +0200

    Navigation between index and detail pages implemented

--- scripts/detail.js
+++ scripts/detail.js
@@ -1,6 +1,38 @@
 'use strict';
 
+let searchParams = new URLSearchParams(window.location.search);
+if (searchParams.has('id')) {
+    loadNote(searchParams.get('id'));
+}
+else {
+    $("#createddatelabel").hide();
+    $("#createddate").hide();
+    $("#finisheddatelabel").hide();
+    $("#finisheddate").hide();
+}
+onImportanceAdjusted();
+
 function onImportanceAdjusted() {
     let template = Handlebars.compile(document.getElementById("importanceitemimagestemplate").innerHTML);
     document.getElementById("importanceitemimages").innerHTML =  template({importance : $("#importanceitem").val()});
+}
+
+function loadNote(id) {
+    console.log('TODO: Load note with ID ' + id);
+
+    $("#title").val('Das ist der Titel der geladenen Notiz');
+    $("#description").val('Das ist die Bezeichnung der geladenen Notiz');
+    $("#importanceitem").val('4');
+    $("#duedate").val('2017-05-21');
+    $("#createddate").val('2000-01-01');
+    $("#finisheddate").val('2017-05-01');
+}
+
+function cancelDetailPage() {
+    window.location.replace('index.html');
+}
+
+function saveNote() {
+    console.log('TODO: Save note');
+    window.location.replace('index.html');
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+function createNewNote() {
+    location.href = 'detail.html';
+}
+
 function finishNote(id, isAlreadyFinished) {
     if (isAlreadyFinished) {
         console.log('TODO: Unfinish note with ID: ' + id);
@@ -10,8 +14,8 @@ function finishNote(id, isAlreadyFinished) {
     reloadNotes();
 }
 
-function editNote(id) {
-    console.log('TODO: Edit note with ID: ' + id);
+function showNoteDetails(id) {
+    location.href = 'detail.html?id=' + id;
 }
 
 function deleteNote(id) {
@@ -49,18 +53,18 @@ function sortNotes(originalData, sortOrder) {
 function getSampleData() {
     return {
         notes: [
-            {id: 1, importance: 1, creationDate: '01.01.2001', dueDate: '01.01.2021', finishDate: '03.05.2017', title: 'Erste Notiz', description: 'Beschreibung der ersten Notiz'},
-            {id: 2, importance: 2, creationDate: '02.02.2002', dueDate: '02.02.2022', finishDate: undefined, title: 'Zweite Notiz', description: 'Beschreibung der zweiten Notiz'},
-            {id: 3, importance: 3, creationDate: '03.03.2003', dueDate: '03.03.2023', finishDate: undefined, title: 'Dritte Notiz', description: 'Beschreibung der dritten Notiz'},
-            {id: 4, importance: 4, creationDate: '04.04.2004', dueDate: '01.01.2024', finishDate: '03.05.2017', title: 'Vierte Notiz', description: 'Beschreibung der vierten Notiz'},
-            {id: 5, importance: 5, creationDate: '05.05.2005', dueDate: '02.02.2025', finishDate: undefined, title: 'Fünfte Notiz', description: 'Beschreibung der fünften Notiz'},
-            {id: 6, importance: 0, creationDate: '06.06.2006', dueDate: '03.03.2026', finishDate: undefined, title: 'Sechste Notiz', description: 'Beschreibung der sechsten Notiz'},
-            {id: 7, importance: 0, creationDate: '07.07.2007', dueDate: '01.01.2027', finishDate: '03.05.2017', title: 'Siebte Notiz', description: 'Beschreibung der siebten Notiz'},
-            {id: 8, importance: 1, creationDate: '08.08.2008', dueDate: '02.02.2028', finishDate: undefined, title: 'Achte Notiz', description: 'Beschreibung der achten Notiz'},
-            {id: 9, importance: 0, creationDate: '09.09.2009', dueDate: '03.03.2029', finishDate: undefined, title: 'Neunte Notiz', description: 'Beschreibung der neunten Notiz'},
-            {id: 10, importance: 0, creationDate: '10.10.2010', dueDate: '01.01.2030', finishDate: '03.05.2017', title: 'Zehnte Notiz', description: 'Beschreibung der zehnten Notiz'},
-            {id: 11, importance: 0, creationDate: '11.11.2011', dueDate: '02.02.2031', finishDate: undefined, title: 'Elfte Notiz', description: 'Beschreibung der elften Notiz'},
-            {id: 12, importance: 0, creationDate: '12.12.2012', dueDate: '03.03.2032', finishDate: undefined, title: 'Zwölfte Notiz', description: 'Beschreibung der zwölften Notiz'}
+            {id: 1, importance: 1, creationDate: '2001-01-01', dueDate: '2021-01-01', finishDate: '2017-05-03', title: 'Erste Notiz', description: 'Beschreibung der ersten Notiz'},
+            {id: 2, importance: 2, creationDate: '2002-02-02', dueDate: '2022-02-02', finishDate: undefined, title: 'Zweite Notiz', description: 'Beschreibung der zweiten Notiz'},
+            {id: 3, importance: 3, creationDate: '2003-03-03', dueDate: '2023-03-03', finishDate: undefined, title: 'Dritte Notiz', description: 'Beschreibung der dritten Notiz'},
+            {id: 4, importance: 4, creationDate: '2004-04-04', dueDate: '2024-04-04', finishDate: '2017-05-03', title: 'Vierte Notiz', description: 'Beschreibung der vierten Notiz'},
+            {id: 5, importance: 5, creationDate: '2005-05-05', dueDate: '2025-05-05', finishDate: undefined, title: 'Fünfte Notiz', description: 'Beschreibung der fünften Notiz'},
+            {id: 6, importance: 0, creationDate: '2006-06-06', dueDate: '2026-06-06', finishDate: undefined, title: 'Sechste Notiz', description: 'Beschreibung der sechsten Notiz'},
+            {id: 7, importance: 0, creationDate: '2007-07-07', dueDate: '2027-07-07', finishDate: '2017-05-03', title: 'Siebte Notiz', description: 'Beschreibung der siebten Notiz'},
+            {id: 8, importance: 1, creationDate: '2008-08-08', dueDate: '2028-08-08', finishDate: undefined, title: 'Achte Notiz', description: 'Beschreibung der achten Notiz'},
+            {id: 9, importance: 0, creationDate: '2009-09-09', dueDate: '2029-09-09', finishDate: undefined, title: 'Neunte Notiz', description: 'Beschreibung der neunten Notiz'},
+            {id: 10, importance: 0, creationDate: '2010-10-10', dueDate: '2030-10-10', finishDate: '2017-05-03', title: 'Zehnte Notiz', description: 'Beschreibung der zehnten Notiz'},
+            {id: 11, importance: 0, creationDate: '2011-11-11', dueDate: '2031-11-11', finishDate: undefined, title: 'Elfte Notiz', description: 'Beschreibung der elften Notiz'},
+            {id: 12, importance: 0, creationDate: '2012-12-12', dueDate: '2032-12-12', finishDate: undefined, title: 'Zwölfte Notiz', description: 'Beschreibung der zwölften Notiz'}
         ]
     };
 }
